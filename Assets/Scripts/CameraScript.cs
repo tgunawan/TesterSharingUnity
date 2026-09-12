77e4c3f3d893f52d10f2b81b320ba3d12618607f
tgunawan <tedi1988@gmail.com>
2026-09-12 15:57:58 +0700

    Update CameraScript and Player Script

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,18 +3,17 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public Transform target;
-    public Transform target2;
     public Vector3 offset;
 
     private void Update()
     {
-        //transform.position = target2.TransformDirection(offset);
-        //transform.rotation = Quaternion.LookRotation((target.position- transform.position), target.forward);
+        transform.position = target.position + target.TransformDirection(offset);
+        transform.rotation = Quaternion.LookRotation((target.position- transform.position), target.forward);
 
         //transform.LookAt(target2);
 
-        transform.position = target.position;
-        transform.rotation = target2.rotation;
+        //transform.position = target.position;
+        //transform.rotation = target.rotation;
 
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,18 +5,44 @@ public class PlayerScript : MonoBehaviour
     public float moveSpeed = 100f;
     public float turnSpeed = 100f;
     private Rigidbody rb;
+    public Transform planetCenter;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+
     private void Update()
     {
-        rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.deltaTime);
 
-        Vector3 turnRotation = Vector3.up * Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        Quaternion eulerRotation = Quaternion.Euler(turnRotation);
-        rb.MoveRotation (eulerRotation * rb.rotation);
+        Vector3 gravityUp = (rb.position - planetCenter.position).normalized;
+
+
+        bool isUpsideDown = Vector3.Dot(gravityUp, Vector3.up) < 0;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (isUpsideDown)
+        {
+            horizontalInput = -horizontalInput;
+        }
+
+
+        Vector3 localTurn = new Vector3(0f, horizontalInput * turnSpeed * Time.deltaTime, 0f);
+        Quaternion eulerRotation = Quaternion.Euler(localTurn);
+        rb.MoveRotation(eulerRotation * rb.rotation);
+
+        // Gerakan maju
+        rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.deltaTime);
     }
+
+
+    //private void Update()
+    //{
+    //    rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.deltaTime);
+
+    //    Vector3 turnRotation = Vector3.up * Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+    //    Quaternion eulerRotation = Quaternion.Euler(turnRotation);
+    //    rb.MoveRotation (eulerRotation * rb.rotation);
+    //}
     private void OnCollisionEnter(Collision collider)
     {
         if (collider.gameObject.tag == "Obstacle")
